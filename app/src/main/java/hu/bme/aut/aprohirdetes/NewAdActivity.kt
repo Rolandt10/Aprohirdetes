52c22f074566e37aa9b08763b93f41b52e978d65
Roland Toth <troland703@gmail.com>
2021-11-25 13:59:06 +0100

    new ad UI

--- app/src/main/java/hu/bme/aut/aprohirdetes/NewAdActivity.kt
+++ app/src/main/java/hu/bme/aut/aprohirdetes/NewAdActivity.kt
@@ -2,12 +2,26 @@ package hu.bme.aut.aprohirdetes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import hu.bme.aut.aprohirdetes.databinding.ActivityMainBinding
+import hu.bme.aut.aprohirdetes.databinding.ActivityNewAdBinding
 import hu.bme.aut.aprohirdetes.databinding.FragmentLoginBinding
 
 class NewAdActivity : AppCompatActivity() {
 
+    private lateinit var binding : ActivityNewAdBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_new_ad)
+        binding = ActivityNewAdBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        val spinner = binding.spinner
+        val adapter = ArrayAdapter.createFromResource(applicationContext, R.array.categories, android.R.layout.simple_spinner_item)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinner.adapter = adapter
     }
 }
