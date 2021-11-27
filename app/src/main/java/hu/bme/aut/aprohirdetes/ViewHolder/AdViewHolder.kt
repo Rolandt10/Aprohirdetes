de0c4de19c497e4c0bb2a4c3c9dcac8e819a34dd
Roland Toth <troland703@gmail.com>
2021-11-27 14:43:44 +0100

    CardView and AdActivity

--- app/src/main/java/hu/bme/aut/aprohirdetes/ViewHolder/AdViewHolder.kt
+++ app/src/main/java/hu/bme/aut/aprohirdetes/ViewHolder/AdViewHolder.kt
@@ -1,16 +1,22 @@
 package hu.bme.aut.aprohirdetes.ViewHolder
 
 import android.view.View
+import android.view.ViewGroup
 import android.widget.TextView
+import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
 import hu.bme.aut.aprohirdetes.R
 
 class AdViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-    var textViewTitle : TextView
-    var textViewCategory : TextView
+    var textViewTitle: TextView
+    var textViewCategory: TextView
+    var textViewPrice: TextView
+    var adItem: ViewGroup
 
     init {
-        textViewTitle = itemView.findViewById(R.id.textViewTitle)
-        textViewCategory = itemView.findViewById(R.id.textViewCategory)
+        textViewTitle = itemView.findViewById(R.id.textTitle)
+        textViewCategory = itemView.findViewById(R.id.textCategory)
+        textViewPrice = itemView.findViewById(R.id.textPrice)
+        adItem = itemView.findViewById(R.id.adItem)
     }
 }
--- app/src/main/java/hu/bme/aut/aprohirdetes/adapter/RecyclerViewAdapter.kt
+++ app/src/main/java/hu/bme/aut/aprohirdetes/adapter/RecyclerViewAdapter.kt
@@ -1,30 +1,59 @@
 package hu.bme.aut.aprohirdetes.adapter
 
+import android.content.Context
+import android.graphics.Color
+import android.provider.Settings.Global.getString
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import hu.bme.aut.aprohirdetes.models.Ad
-import hu.bme.aut.aprohirdetes.R
 import hu.bme.aut.aprohirdetes.ViewHolder.AdViewHolder
+import hu.bme.aut.aprohirdetes.R
+
 
 class RecyclerViewAdapter(private val ads: MutableList<Ad?>) : RecyclerView.Adapter<AdViewHolder>() {
+
+    private lateinit var context: Context
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AdViewHolder {
         val view = LayoutInflater.from(parent.context)
             .inflate(R.layout.ad_item, parent, false)
+        context = parent.context
         return AdViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: AdViewHolder, position: Int) {
-        holder.textViewTitle.text = ads.get(position)?.title.toString()
-        holder.textViewCategory.text = ads.get(position)?.category.toString()
+        val title = ads.get(position)?.title.toString()
+        val category = ads.get(position)?.category.toString()
+        val price = ads.get(position)?.price.toString()
+
+        holder.textViewTitle.text = title
+        holder.textViewCategory.text = category
+        holder.textViewPrice.text = context.getString(R.string.price_in_HUF, price)
+
+        when (category) {
+            "Álláskeresés, állásajánlat" -> {
+                holder.adItem.setBackgroundColor(Color.rgb(253, 253, 155))
+            }
+            "Otthon, háztartás" -> {
+                holder.adItem.setBackgroundColor(Color.rgb(249, 181, 139))
+            }
+            "Műszaki cikkek" -> {
+                holder.adItem.setBackgroundColor(Color.rgb(220, 229, 228))
+            }
+            "Sport, szabadidő" -> {
+                holder.adItem.setBackgroundColor(Color.rgb(155, 119, 253))
+            }
+            "Ruházat" -> {
+                holder.adItem.setBackgroundColor(Color.rgb(211, 206, 222))
+            }
+            "Üzlet, szolgáltatás" -> {
+                holder.adItem.setBackgroundColor(Color.rgb(156, 231, 252))
+            }
+        }
     }
 
     override fun getItemCount(): Int {
         return ads.size
-    }
-
-    fun addAds(ads: MutableList<Ad?>) {
-        ads.addAll(ads)
     }
 }
